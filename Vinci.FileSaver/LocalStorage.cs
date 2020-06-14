54432a5c5ccee9f12b9410e99982cf18270e935a
qianzhongxiang <qianzhongxiang@outlook.com>
2020-06-14 22:19:10 +0800

    first created

---
+++ Vinci.FileSaver/LocalStorage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Vinci.FileSaver
+{
+    public class LocalStorage
+    {
+        DirectoryInfo RootDirectory = null;
+
+        internal LocalStorage()
+        {
+
+        }
+        public bool SaveImage(Image img)
+        {
+
+        }
+
+        public Image GetImage(string id)
+        {
+
+        }
+
+
+        public bool SaveFile()
+        {
+
+        }
+
+        public FileStream GetFile(string id)
+        {
+            GetDictionary
+        }
+
+        public void SetRootDirectory(DirectoryInfo dir)
+        {
+            RootDirectory = dir;
+        }
+        //LocalConfiguration _config;
+        //public LocalConfiguration MyProperty { get => _config ?? (_config = new LocalConfiguration()); }
+    }
+}
---
+++ Vinci.FileSaver/PathResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Vinci.FileSaver
+{
+    class PathResolver
+    {
+        public string GetPathId()
+        {
+            var now = DateTime.UtcNow;
+        return now.ToString("yyyy-MM-dd-mm-ss-") $"{now.Year}-{now.Month}-{now.}"
+            return string.Empty;
+        }
+
+        public DirectoryInfo GetDictionary(DirectoryInfo root, string Id)
+        {
+            var subPath = Id.Replace('-', '\\');
+            return Directory.CreateDirectory(Path.Combine(root.FullName, subPath));
+        }
+    }
+}
