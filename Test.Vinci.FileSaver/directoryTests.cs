2ddb85cb1c46ce9f71ab726830520879195abb53
qianzhongxiang <qianzhongxiang@outlook.com>
2020-07-16 08:51:19 +0100

    add  copyto extension of directory

---
+++ Test.Vinci.FileSaver/directoryTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Vinci.FileSaver.Extension;
+
+namespace Test.Vinci.FileSaver
+{
+    public class DirectoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        [Test]
+        public void CopyTo()
+        {
+            var testDir = Directory.CreateDirectory("testDir");
+            var f1 = File.Create(Path.Combine(testDir.FullName, "t1.text"), 20);
+            f1.Close();
+            var subDir = testDir.CreateSubdirectory("subDir");
+            var f2 = File.Create(Path.Combine(subDir.FullName, "t2.text"), 20);
+            f2.Close();
+            testDir.CopyTo("destDir", true);
+            var destDir = new DirectoryInfo("destDir");
+            Assert.IsTrue(destDir.Exists);
+            subDir = destDir.GetDirectories()[0];
+            Assert.AreEqual("subDir", subDir.Name);
+            Assert.AreEqual(destDir.GetFiles()[0].Name, "t1.text");
+            Assert.AreEqual(subDir.GetFiles()[0].Name, "t2.text");
+            destDir.Delete(true);
+            testDir.Delete(true);
+            Assert.Pass();
+        }
+    }
+}
---
+++ Vinci.FileSaver/Extension/Directory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Vinci.FileSaver.Extension
+{
+    public static class DirectoryEx
+    {
+        /// <summary>
+        /// Copy to extension
+        /// </summary>
+        /// <param name="sourceDir"></param>
+        /// <param name="destDirName"></param>
+        /// <param name="copySubDirs"> </param>
+        public static void CopyTo(this DirectoryInfo sourceDir, string destDirName, bool copySubDirs = false)
+        {
+            // Get the subdirectories for the specified directory.
+            if (!sourceDir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDir.FullName);
+            }
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = sourceDir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                DirectoryInfo[] dirs = sourceDir.GetDirectories();
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    subdir.CopyTo(temppath, copySubDirs);
+                }
+            }
+        }
+    }
+}
