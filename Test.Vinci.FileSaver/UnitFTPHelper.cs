57307f3fb66cc7021636e6c5f03f43a53bea84f0
qianzhongxiang <qianzhongxiang@outlook.com>
2020-11-24 02:29:40 +0000

    add  ftp list function

---
+++ Test.Vinci.FileSaver/UnitFTPHelper.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Vinci.FileSaver;
+using Vinci.FileSaver.Interface;
+
+namespace Test.Vinci.FileSaver
+{
+    public class UnitFTPHelper
+    {
+        private IStorage _Storage;
+        [SetUp]
+        public void Setup()
+        {
+            _Storage = Storage.FTP.Build("ftp://192.168.10.204", "inspack", "123456@q");
+        }
+
+
+        [Test]
+        public void List()
+        {
+
+            FTPHelper.Clear(_Storage);
+            using (var file = File.OpenRead("userMgr.ico"))
+            {
+                _Storage.SaveFile(file, "ico", null, $"newAdd_1");
+                _Storage.SaveFile(file, "ico", null, $"newAdd_2");
+                _Storage.SaveFile(file, "ico", null, $"newAdd_3");
+            }
+
+            var strs = FTPHelper.ListFile(_Storage);
+            Assert.AreEqual(3, strs.Length);
+            foreach (var name in strs)
+            {
+                Assert.IsTrue(name.StartsWith("newAdd_"));
+                Assert.IsTrue(name.EndsWith(".ico"));
+            }
+            Assert.Pass();
+        }
+    }
+}
---
+++ Vinci.FileSaver/FTPHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vinci.FileSaver.Interface;
+using System.Linq;
+
+namespace Vinci.FileSaver
+{
+    public static class FTPHelper
+    {
+
+        public static string[] ListFile(IStorage storage, string rootDir = null)
+        {
+            if (storage is FTPStorage)
+            {
+                var ftpClient = (storage as FTPStorage).Client;
+                FluentFTP.FtpListItem[] items;
+                if (string.IsNullOrWhiteSpace(rootDir)) {
+                    items= ftpClient.GetListing(".");
+                }
+                else
+                {
+                    items = ftpClient.GetListing(rootDir);
+                }
+                return items.Where(i=>i.Type== FluentFTP.FtpFileSystemObjectType.File&&!i.Name.StartsWith(".")).Select(i => i.Name).ToArray();
+                //return ftpClient.GetNameListing(rootDir);
+            }
+            else
+            {
+                throw new ArgumentException("argument 'storage' is not a validate FTP Storage", nameof(storage));
+            }
+        }
+
+        public static void Clear(IStorage storage, string rootDir = null)
+        {
+            if (storage is FTPStorage)
+            {
+                var ftpClient = (storage as FTPStorage).Client;
+                var files = ListFile(storage, rootDir);
+                foreach (var item in files)
+                {
+                    ftpClient.DeleteFile(item);
+                }
+            }
+            else
+            {
+                throw new ArgumentException("argument 'storage' is not a validate FTP Storage", nameof(storage));
+            }
+        }
+    }
+}
